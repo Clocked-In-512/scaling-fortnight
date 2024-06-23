d8cf407b213d1631d8e637e46a192cfa31608659
Clocked-In-512 <robert.fernandez3@g.austincc.edu>
2024-06-23 15:11:59 -0500

    Add files via upload
    This is the distance module you have to call in order to run your distance calculation for the code.

---
+++ distance.py
@@ -0,0 +1,8 @@
+##Robert Fernandez
+##Complete
+##This program will show the falling distance of an object.
+
+def fallingDistance(fallingtime):
+    gravity = 9.8
+    distance = (1/2) * gravity * (fallingtime ** 2)
+    return distance
